7560f5ca770cd20e29b1ecf096967dbdb292bf49
Matthew Martin <zhintateo19924559@gmail.com>
2020-11-07 15:12:46 -0600

    changed to be push on heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const server = require('./server');
-const port = 4000;
+const port = process.env.PORT || 4000;
 server.listen(port, ()=>{
     console.log(`\n***Magic happening on port ${port}***\n`)
 })
