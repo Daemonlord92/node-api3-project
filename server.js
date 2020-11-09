584f26aa6c33453e5d34a7794ef808bf1342b430
Matthew Martin <zhintateo19924559@gmail.com>
2020-11-08 19:09:30 -0600

    Update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ server.use('/posts', logger, postRouter);
 server.use('/users', logger, userRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.status(200).json({mes: "HI, from the backend"});
 });
 
 //custom middleware
